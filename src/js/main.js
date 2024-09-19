9b715e8ee2ee3d3daf73dd4912ca35ff0eec7335
Ruby <pineapples1104@gmail.com>
2024-09-19 11:17:42 -0600

    start of nps1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+document.querySelector('title').innerHTML = parkData.fullName;
+
+let disclaimer = document.querySelector('.disclaimer a');
+
+disclaimer.innerHTML = parkData.fullName;
+disclaimer.href = parkData.url;
+
+// ADJUST AS HTML IS MADE
+// document.querySelector('.hero-banner-title').innerHTML = parkData.name;
+
+// let heroBannerImg =document.querySelector('.hero-banner img')
+// heroBannerImg.src = parkData.images[0].url;
+// heroBannerImg.alt = parkData.images[0].altText;
