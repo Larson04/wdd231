967100133fa9bb97741b58ea30bf5feb6b7ac5df
Ruby <pineapples1104@gmail.com>
2024-10-31 11:28:50 -0600

    nps 5

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,9 @@ async function init() {
     createMediaCard(links);
     setHeaderFooter(parkData);
     introText(parkData);
+    enableNavigation();
 }
   
-init();
 
 function introText(data){
     console.log(data);
@@ -29,4 +29,25 @@ function createMediaCard(data){
     mediaCards.innerHTML = html.join('');
 }
 
+function enableNavigation(){
+    const menuButton = document.querySelector('#global-nav-toggle');
 
+    menuButton.addEventListener('click', (ev) => {
+        let target = ev.target;
+        console.log("click");
+        document.querySelector('.global-nav').classList.toggle('show');
+
+        if (target.tagName != 'BUTTON') {
+            target = target.closest('button');
+        }
+        if (document.querySelector('.global-nav').classList.contains('show')) {
+            target.setAttribute('aria-expanded', 'true');
+        } else {
+            target.setAttribute('aria-expanded', 'false');
+        }
+
+        console.log('toggle');
+    });
+}
+
+init();
